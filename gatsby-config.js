94dfbc18a6dbf9281077c5432ba1558444dea25f
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-01-03 11:04:50 +0800

    :heavy_plus_sign: inject gatsby-plugin-minify-html

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
     },
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-minify-html',
     'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
