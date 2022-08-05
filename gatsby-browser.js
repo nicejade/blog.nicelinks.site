0edc554f75031d707926686bf51c26f59aa6536f
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-08-05 23:42:56 +0800

    :sparkles: add extra link: abblock detect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+const addScript = url => {
+	const script = document.createElement("script")
+	script.src = url
+	script.async = true
+	document.body.appendChild(script)
+}
+
+export const onClientEntry = () => {
+	window.onload = () => {
+		addScript("https://image.nicelinks.site/ad-block-remind-min.js?v07")
+    setTimeout(() => {
+      var AdBlockRemind = document.createElement("ad-block-remind");
+      document.body.appendChild(AdBlockRemind);
+    }, 600)
+	}
+}
