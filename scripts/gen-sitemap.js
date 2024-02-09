bbc1c0b7340300741197c56fabc136459f778fe5
JadeYang <jeffygisgreat@gmail.com>
2024-02-09 16:48:46 +0800

    :lipstick: modify page style for ADS

--- scripts/gen-sitemap.js
+++ scripts/gen-sitemap.js
@@ -1,24 +1,23 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
 
-const sitemapTempStr =
-  `<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
+const sitemapTempStr = `<?xml version="1.0" encoding="utf-8" standalone="yes" ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://blog.nicelinks.site/</loc></url>
   #PLACEHOLDER#
 </urlset>`
 
 const generate = async () => {
-  const baseUrl = 'https://blog.nicelinks.site'
+  const baseUrl = "https://blog.nicelinks.site"
   const linksUrlArr = []
-  const files = fs.readdirSync(path.join(__dirname, '..', 'src/blogs'))
-  files.forEach((item) => {
-    const name = item.replace('.md', '')
+  const files = fs.readdirSync(path.join(__dirname, "..", "src/blogs"))
+  files.forEach(item => {
+    const name = item.replace(".md", "")
     const link = `${baseUrl}/${name}/`
     const urlLocItem = `<url><loc>${link}</loc></url>`
     linksUrlArr.push(urlLocItem)
   })
-  const linksUrlStr = linksUrlArr.join('\n  ')
+  const linksUrlStr = linksUrlArr.join("\n  ")
   const sitemapContent = sitemapTempStr.replace(`#PLACEHOLDER#`, linksUrlStr)
   const outputPath = `${process.cwd()}/public/sitemap.xml`
   fs.writeFileSync(outputPath, sitemapContent)
